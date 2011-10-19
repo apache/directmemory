2850990fb8da38fb9da3b7bd0af6f999952a4d2f
Ioannis Canellos <iocanel@apache.org>
2011-10-19 20:43:00 +0000

    [DIRECTMEMORY-22] Added pretty basic osgi integration tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directmemory/trunk@1186472 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itests/osgi/src/test/java/org/apache/directmemory/tests/osgi/cache/CacheTest.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.directmemory.tests.osgi.cache;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.directmemory.cache.Cache;
+import org.apache.directmemory.measures.Every;
+import org.apache.directmemory.measures.Monitor;
+import org.apache.directmemory.measures.Ram;
+import org.apache.directmemory.memory.Pointer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.ops4j.pax.exam.Customizer;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Constants;
+
+
+import static org.junit.Assert.*;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.OptionUtils.expand;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.workingDirectory;
+import static org.ops4j.pax.swissbox.tinybundles.core.TinyBundles.modifyBundle;
+
+@RunWith(JUnit4TestRunner.class)
+public class CacheTest {
+
+  /**
+   * This tests basic cache operations(put,retrieve) inside OSGi
+   */
+  @Test
+  public void testWithString() {
+    String key = "1";
+    String obj = "Simple String Object";
+    Cache.init(1, Ram.Mb(16));
+		Cache.scheduleDisposalEvery(Every.seconds(1));
+		Cache.dump();
+
+    Pointer p = Cache.put("1", obj);
+    Object result = Cache.retrieve("1");
+
+    Cache.dump();
+    Monitor.dump("cache");
+
+    assertEquals(obj, result);
+  }
+
+  /**
+   * This test basic cache operations(put,retrieve) inside OSGi using an object of an imported class (provided by an other bundle).
+   */
+  @Test
+  public void testWithImportedObject() {
+    SimpleObject obj1 = new SimpleObject("1","Object One");
+    SimpleObject obj2 = new SimpleObject("2","Object Two");
+    Cache.init(1, Ram.Mb(16));
+		Cache.scheduleDisposalEvery(Every.seconds(1));
+		Cache.dump();
+
+    Pointer p1 = Cache.put("1",obj1 );
+    Pointer p2 = Cache.put("2",obj2 );
+    Object result1 = Cache.retrieve("1");
+    Object result2 = Cache.retrieve("2");
+
+    Cache.dump();
+    Monitor.dump("cache");
+
+    assertEquals(obj1, result1);
+    assertEquals(obj2, result2);
+  }
+
+
+  @Configuration
+  public Option[] configure() {
+    return expand(
+            mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.configadmin").version("1.2.8"),
+            mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-api").version("1.6.2"),
+            mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-service").version("1.6.2"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.guava").version("09_1"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.ant").version("1.7.0_5"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.ant").version("1.7.0_5"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.ant").version("1.7.0_5"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.oro").version("2.0.8_5"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.josql").version("1.5_5"),
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.aspectj").version("1.6.8_2"),
+            mavenBundle().groupId("com.dyuproject.protostuff").artifactId("protostuff-uberjar").version("1.0.2"),
+            mavenBundle().groupId("org.apache.directmemory").artifactId("directmemory-cache").version("0.5.5-SNAPSHOT"),
+            new Customizer() {
+                    @Override
+                    public InputStream customizeTestProbe(InputStream testProbe) {
+                        return modifyBundle(testProbe)
+                                .add(SimpleObject.class)
+                                .set(Constants.DYNAMICIMPORT_PACKAGE, "*")
+                                .build();
+                    }
+                },
+            //Uncomment the line below to debug test
+            vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
+            felix(),
+            equinox()
+    );
+  }
+}
