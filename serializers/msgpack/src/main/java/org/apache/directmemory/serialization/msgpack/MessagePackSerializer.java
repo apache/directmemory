0f4a7ad4f4aac9b26dd1f608b139e2e9e5294812
Simone Tripodi <simonetripodi@apache.org>
2012-02-15 20:54:43 +0000

    initial (basic) version of MsgPack serializer adapter - not complete, but at least a starting point
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directmemory/trunk@1244715 13f79535-47bb-0310-9956-ffa450edef68

---
+++ serializers/msgpack/src/main/java/org/apache/directmemory/serialization/msgpack/MessagePackSerializer.java
@@ -0,0 +1,55 @@
+package org.apache.directmemory.serialization.msgpack;
+
+import java.io.IOException;
+
+import org.apache.directmemory.serialization.Serializer;
+import org.kohsuke.MetaInfServices;
+import org.msgpack.MessagePack;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+@MetaInfServices
+public final class MessagePackSerializer
+    implements Serializer
+{
+
+    private final MessagePack msgpack = new MessagePack();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T> byte[] serialize( T obj )
+        throws IOException
+    {
+        return msgpack.write( obj );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T> T deserialize( byte[] source, Class<T> clazz )
+        throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException
+    {
+        return msgpack.read( source, clazz );
+    }
+
+}
