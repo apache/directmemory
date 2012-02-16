7be1c4204f26475542c9509a966215cfb8727d5e
Simone Tripodi <simonetripodi@apache.org>
2012-02-16 15:01:57 +0000

    magic number
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directmemory/trunk@1245011 13f79535-47bb-0310-9956-ffa450edef68

--- directmemory-cache/src/main/java/org/apache/directmemory/measures/Sizing.java
+++ directmemory-cache/src/main/java/org/apache/directmemory/measures/Sizing.java
@@ -23,19 +23,22 @@ import static java.lang.String.format;
 
 public class Sizing
 {
+
+    private static final int KILOBYTE_UNIT = 1024;
+
     public static int Gb( double giga )
     {
-        return (int) giga * 1024 * 1024 * 1024;
+        return (int) giga * KILOBYTE_UNIT * KILOBYTE_UNIT * KILOBYTE_UNIT;
     }
 
     public static int Mb( double mega )
     {
-        return (int) mega * 1024 * 1024;
+        return (int) mega * KILOBYTE_UNIT * KILOBYTE_UNIT;
     }
 
     public static int Kb( double kilo )
     {
-        return (int) kilo * 1024;
+        return (int) kilo * KILOBYTE_UNIT;
     }
 
     public static int unlimited()
@@ -45,17 +48,17 @@ public class Sizing
 
     public static String inKb( long bytes )
     {
-        return format( "%(,.1fKb", (double) bytes / 1024 );
+        return format( "%(,.1fKb", (double) bytes / KILOBYTE_UNIT );
     }
 
     public static String inMb( long bytes )
     {
-        return format( "%(,.1fMb", (double) bytes / 1024 / 1024 );
+        return format( "%(,.1fMb", (double) bytes / KILOBYTE_UNIT / KILOBYTE_UNIT );
     }
 
     public static String inGb( long bytes )
     {
-        return format( "%(,.1fKb", (double) bytes / 1024 / 1024 / 1024 );
+        return format( "%(,.1fKb", (double) bytes / KILOBYTE_UNIT / KILOBYTE_UNIT / KILOBYTE_UNIT );
     }
 
 }
