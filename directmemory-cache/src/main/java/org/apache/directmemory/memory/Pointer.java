274602a00f769f9fbd6fa1661cc1c06ed1b4f0f7
Simone Tripodi <simonetripodi@apache.org>
2012-02-26 17:24:28 +0000

    just relocated license header location
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directmemory/trunk@1293882 13f79535-47bb-0310-9956-ffa450edef68

--- directmemory-cache/src/main/java/org/apache/directmemory/memory/Pointer.java
+++ directmemory-cache/src/main/java/org/apache/directmemory/memory/Pointer.java
@@ -1,7 +1,5 @@
 package org.apache.directmemory.memory;
 
-import java.nio.ByteBuffer;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -10,9 +8,9 @@ import java.nio.ByteBuffer;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,6 +18,8 @@ import java.nio.ByteBuffer;
  * specific language governing permissions and limitations
  * under the License.
  */
+
+import java.nio.ByteBuffer;
 
 public interface Pointer<T>
 {
@@ -61,7 +61,7 @@ public interface Pointer<T>
     void setDirectBuffer( ByteBuffer directBuffer );
 
     void createdNow();
-    
+
     void setExpiration( long expires, long expiresIn );
 
 }
