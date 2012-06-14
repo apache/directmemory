2be54149ce6889109279b27a138cd03b75ceb1f9
Olivier Lamy <olamy@apache.org>
2012-06-14 07:00:33 +0000

    gloups
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directmemory/trunk@1350106 13f79535-47bb-0310-9956-ffa450edef68

--- directmemory-cache/src/main/java/org/apache/directmemory/DirectMemory.java
+++ directmemory-cache/src/main/java/org/apache/directmemory/DirectMemory.java
@@ -51,7 +51,7 @@ public final class DirectMemory<K, V>
 
     private int numberOfBuffers;
 
-    private int sizes;
+    private int size;
 
     private int initialCapacity = DEFAULT_INITIAL_CAPACITY;
 
