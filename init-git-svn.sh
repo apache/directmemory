4e4df537dc8c68262da2fd26171eec06a33f1ee6
Olivier Lamy <olamy@apache.org>
2012-08-21 16:01:24 +0000

    fix more incubator links
    git-svn-id: https://svn.apache.org/repos/asf/directmemory/trunk@1375633 13f79535-47bb-0310-9956-ffa450edef68

--- init-git-svn.sh
+++ init-git-svn.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 cd .git;wget http://git.apache.org/authors.txt; cd ..
 git config svn.authorsfile ".git/authors.txt"
-git svn init --prefix=origin/ --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/incubator/directmemory/
+git svn init --prefix=origin/ --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/directmemory/
 git svn rebase
