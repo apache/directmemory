ff3f4a0c32c0b0f81f9922869018fc130b12d3c2
Christoph Engelbert <noctarius@apache.org>
2012-10-28 19:44:52 +0000

    Ignored benchmark unittest for Unsafe since the functionality is tested by another unittest
    git-svn-id: https://svn.apache.org/repos/asf/directmemory/trunk@1403085 13f79535-47bb-0310-9956-ffa450edef68

--- directmemory-cache/src/test/java/org/apache/directmemory/memory/MallocWithUnsafeTest.java
+++ directmemory-cache/src/test/java/org/apache/directmemory/memory/MallocWithUnsafeTest.java
@@ -29,12 +29,14 @@ import java.util.concurrent.ConcurrentMap;
 import org.apache.directmemory.measures.Ram;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.MapMaker;
 
+@Ignore
 public class MallocWithUnsafeTest
 {
 
